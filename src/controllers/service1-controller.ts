611fef683bb7ab64830b09da8352760375989af2
Timo Vormweg <timo.vormweg@i-flow.io>
2024-06-28 09:55:30 +0000

    Update src/controllers/services-controller.ts, src/controllers/health-controller.ts, src/controllers/service1-controller.ts, src/controllers/service2-controller.ts files

--- src/controllers/service1-controller.ts
+++ src/controllers/service1-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 
 export const service1Controller = (req: Request, res: Response) => {
-    res.json({ message: req.query.param1 });
+    res.json({ message: req.query.param1, info: {
+        "mat_nr": "1234"
+    }});
 };
--- src/controllers/services-controller.ts
+++ src/controllers/services-controller.ts
@@ -4,32 +4,38 @@ import { IAnyConnectServiceDefinitions } from "../types";
 export const servicesController = (req: Request, res: Response) => {
     const services: IAnyConnectServiceDefinitions[] = [
         {
-            name: "Service 1",
-            description: "Description of Service 1",
+            name: "GetMaterialIdInfo",
+            description: "Get data for Material ID from ERP.",
             url: "/service1",
             method: "GET",
             query: [
                 {
-                    name: "param1",
+                    name: "material_id",
                     type: "string",
-                    description: "Description of param1",
-                    example: "example1"
+                    description: "The material id.",
+                    example: "MAT_345634"
                 }
             ],
             body: []
         },
         {
-            name: "Service 2",
-            description: "Description of Service 2",
+            name: "UpdateStock",
+            description: "Update stock count of material within ERP",
             url: "/service2",
             method: "POST",
             query: [],
             body: [
                 {
-                    name: "param1",
+                    name: "new_stock",
+                    type: "number",
+                    description: "The new stock of :material_id",
+                    example: "125"
+                },
+                {
+                    name: "material_id",
                     type: "string",
-                    description: "Description of param1",
-                    example: "example1"
+                    description: "The material id.",
+                    example: "MAT_345634"
                 }
             ]
         }
